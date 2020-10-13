129402d345fcd11ab9582ebcbfe1859f863195fe
lizlaffitte <laffitte.digital@gmail.com>
2020-10-13 18:56:25 -0400

    Communicate with MailChimp API.

--- src/actions/campaignActions.js
+++ src/actions/campaignActions.js
@@ -1,10 +1,3 @@
-export const addReview = review => {
-    return {
-        type: "ADD_REVIEW",
-        review
-    }
-}
-
 export const addCampaigns = campaigns => {
     return {
         type: "ADD_CAMPAIGNS",
@@ -14,18 +7,7 @@ export const addCampaigns = campaigns => {
 
 
 
-export const setCurrentReview = review => {
-    return {
-        type: "SET_CURRENT_REVIEW",
-        review
-    }
-}
 
-export const clearCurrentReview = () => {
-    return {
-        type: "CLEAR_CURRENT_REVIEW"
-    }
-}
 
 //Async
 
