b6d61b185bf3600c34323ac00f326b81da291dd3
lizlaffitte <laffitte.digital@gmail.com>
2020-10-17 15:18:31 -0400

    Connect App to store, so we can fetch campaigns and find the correct one when visiting report/:id path.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import './App.css';
 import Campaigns from './components/Campaigns'
+import {Component} from 'react'
+import Report from './components/Report'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {fetchCampaigns} from './actions/campaignActions'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>MailChimp Reports</h1>
-        <Campaigns />
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchCampaigns()
 }
 
-export default App;
+  findCampaign = (id) =>{
+    return (this.props.campaigns.find(campaign => campaign.id == id))
+  }
+
+  render(){
+    return (
+      <Router>
+      <div className="App">
+        <h1>MailChimp Reports</h1>
+        <Switch>
+          <Route exact path='/' render={props => <Campaigns {...props} /> } />
+          <Route exact path="/reports/:id" render={props => <Report {...props} campaign={this.findCampaign(props.match.params.id)}/>} />
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
+
+}
+const mapStateToProps = ({campaigns}) => ({campaigns})
+export default connect(mapStateToProps, {fetchCampaigns})(App)
