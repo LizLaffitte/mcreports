69fd8c5f1fcea31021c5dd3ed9c4119f8ac98b48
lizlaffitte <laffitte.digital@gmail.com>
2020-10-17 14:08:07 -0400

    Add See Report button after each campaign. Pull onClick prop into a function to keep from immediately invoking.

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import {fetchCampaigns} from '../actions/campaignActions'
+import Button from './Button'
 
 class Campaigns extends Component{
     componentDidMount(){
@@ -8,9 +9,13 @@ class Campaigns extends Component{
         
     }
 
+    getReport = () =>{
+        console.log("Click")
+    }
+
     renderCampaigns = () => {
         return this.props.campaigns.map(campaign => {
-            return(<li>{campaign.campaign_title}</li>)
+            return(<li>{campaign.campaign_title} <Button text="See Report" click={this.getReport} /></li>)
         })
 
     }
