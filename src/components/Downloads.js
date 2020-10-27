97f369798f10e1e1c66d7898fbe31256b1c6ce4a
lizlaffitte <laffitte.digital@gmail.com>
2020-10-27 06:14:04 -0400

    Testing mixing ExcelSheet components.

---
+++ src/components/Downloads.js
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactExport from "react-export-excel";
+
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
+const ExcelCell = ReactExport.ExcelFile.ExcelCell
+const dataSet1 = [
+    {
+        name: "Johson",
+        amount: 30000,
+        sex: 'M',
+        is_married: true
+    },
+    {
+        name: "Monika",
+        amount: 355000,
+        sex: 'F',
+        is_married: false
+    },
+    {
+        name: "John",
+        amount: 250000,
+        sex: 'M',
+        is_married: false
+    },
+    {
+        name: "Josef",
+        amount: 450500,
+        sex: 'M',
+        is_married: true
+    }
+];
+
+const multiDataSet = [
+    {
+        columns: [
+            {value: "Headings"}
+        ],
+        data: [
+            [
+                {value: "Test"}
+            ]
+        ]
+    }
+]
+
+
+class Downloads extends React.Component {
+    render() {
+        return (
+            <ExcelFile element={<button>Download Email Report</button>} filename="Email Campaign Report" >
+                <ExcelSheet data={dataSet1} name="Email Report">
+                    <ExcelColumn label="Name" value="name"/>
+                    <ExcelColumn label="Wallet Money" value="amount"/>
+                    <ExcelColumn label="Gender" value="sex"/>
+                    <ExcelColumn label="Marital Status"
+                                 value={(col) => col.is_married ? "Married" : "Single"}/>
+                </ExcelSheet>
+                {/* <ExcelSheet data={dataSet2} name="Leaves">
+                    <ExcelColumn label="Name" value="name"/>
+                    <ExcelColumn label="Total Leaves" value="total"/>
+                    <ExcelColumn label="Remaining Leaves" value="remaining"/>
+                </ExcelSheet> */}
+                <ExcelSheet data={multiDataSet} name="Test" />
+            </ExcelFile>
+        );
+    }
+}
+
+export default Downloads
