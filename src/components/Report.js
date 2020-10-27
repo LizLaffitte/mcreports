b9af022a957c54d0e38c93980630b7597cd1dd73
lizlaffitte <laffitte.digital@gmail.com>
2020-10-27 06:16:03 -0400

    Correctly label report age and pass Downloads component campaign data.

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-// import {connect} from 'react-redux'
-// import {fetchCampaigns} from '../actions/campaignActions'
-// import Button from './Button'
+import {Link} from 'react-router-dom'
+import Button from './Button'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Downloads from './Downloads'
+import Test from './Test'
 
+const reportData = {
+    title: 'title'      
+}
 class Report extends Component {
     
     render(){
         return(
+            <div>
+                <h2>Email Campaign Report</h2>
             <p>
                 {this.props.campaign.campaign_title}
                 {console.log(this.props.campaign)}
             </p>
+            {/* <Downloads data={this.props.campaign} /> */}
+            <Test />
+            </div>
         )
         
     }
